0d087f88d87d1714176a8398caa5579ffd69f583
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-11-07 13:29:56 +0300

    test_unit

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ from file_job import *
 class test(unittest.TestCase):
     def test_true(self):
         self.assertTrue(R("mama"))
+        self.assertTrue(R("nn"))
 
     def test_empty_string(self):
-        self.assertFalse(R("papa"))
+        self.assertFalse(R("paa"))
+        self.assertFalse(R("  "))
         
