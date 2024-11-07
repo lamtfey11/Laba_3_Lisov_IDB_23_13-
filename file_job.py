df11d6a33c442dbf6a0f44df8bcc00d1d5b87fbc
Матфей Лисов Игоревич <lmatfey11@gmail.com>
2024-11-07 04:14:37 +0300

    print_add_enter

--- file_job.py
+++ file_job.py
@@ -3,11 +3,12 @@ import re
 class file_job:
     def print_and_add_and_enter(self):
         word = input('Введите своё слово для добавления в файл: ')
+        word += "\n"
+        with open("file.txt", "a") as file:
+            file.write(word)
         
-        with open("file.txt", "a") as myfile:
-            myfile.write(word)
-        
-        
+        with open('file.txt', 'r') as file:
+            print(file.read())
 
     def check(self):
         print('check')
